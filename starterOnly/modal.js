9d19db8bb142c7468b702a71ecaf65ef5269ae9f
Poccardl <loic.poccard@gmail.com>
2020-12-08 15:50:13 +0100

    Issue 2 75%

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,10 +12,19 @@ const modalbg = document.querySelector(".bground");
 const closemodal = document.querySelector(".close");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+console.log(formData[0]);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 closemodal.addEventListener("click",  closeModal);
+//formData.forEach((input) => input.addEventListener("change", takeData));
+formData[0].addEventListener("change", firstData);
+formData[1].addEventListener("change", lastData);
+formData[2].addEventListener("change", emailData);
+formData[3].addEventListener("change", birthdateData);
+formData[4].addEventListener("change", quantityData);
+
+
 
 // launch modal form
 function launchModal() {
@@ -24,5 +33,25 @@ function launchModal() {
 // close modal form
 function closeModal() {
   modalbg.style.display = "none";
+}
+// keep first name data
+function firstData(e) {
+  console.log("firstData ->", e.target.value);
+}
+// keep last name data
+function lastData(e) {
+  console.log("lastData ->", e.target.value);
+}
+// keep email data
+function emailData(e) {
+  console.log("emailData ->", e.target.value);
+}
+// keep brithday date data
+function birthdateData(e) {
+  console.log("birthdateData ->", e.target.value);
+}
+// kepp quantity data
+function quantityData(e) {
+  console.log("quantityData ->", e.target.value);
 }
 
