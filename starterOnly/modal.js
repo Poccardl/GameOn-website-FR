d862e476b4cf6c5adca37c030b42420b04bac247
Poccardl <loic.poccard@gmail.com>
2020-12-11 13:49:49 +0100

    Issue 2 rework 100%

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,21 +11,31 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const closemodal = document.querySelector(".close");
 const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-console.log(formData[0]);
+const formData = document.querySelectorAll(".formData input");
+
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 closemodal.addEventListener("click",  closeModal);
-//formData.forEach((input) => input.addEventListener("change", takeData));
+
+// launch form event
 formData[0].addEventListener("change", firstData);
 formData[1].addEventListener("change", lastData);
 formData[2].addEventListener("change", emailData);
 formData[3].addEventListener("change", birthdateData);
 formData[4].addEventListener("change", quantityData);
+formData[5].addEventListener("change", locationData);
+formData[6].addEventListener("change", locationData);
+formData[7].addEventListener("change", locationData);
+formData[8].addEventListener("change", locationData);
+formData[9].addEventListener("change", locationData);
+formData[10].addEventListener("change", locationData);
+console.log(formData)
+formData[11].addEventListener("change", conditionData);
+formData[12].addEventListener("change", conditionData);
 
 
-
+/* FUNCTION */
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
@@ -34,25 +44,99 @@ function launchModal() {
 function closeModal() {
   modalbg.style.display = "none";
 }
-// keep first name data
+
+// first name validation
 function firstData(e) {
-  console.log("firstData ->", e.target.value);
+  // on sible la classe pour la mètre en rouge
+  console.log("firstData :", e.target.value);
+  var data = e.target.value;
+  var valid = false;
+  if (data.length > 1) {
+    valid = true;
+    formData[0].style.borderColor = "green";
+  }
+  else {
+    valid = false
+    formData[0].style.borderColor = "red";
+  }
 }
-// keep last name data
+// last name validation
 function lastData(e) {
-  console.log("lastData ->", e.target.value);
+  console.log("data", data);
+  // on sible la classe pour la mètre en rouge
+  console.log("lastData :", e.target.value);
+  var data = e.target.value;
+  var valid = false;
+  if (data.length > 1) {
+    valid = true;
+    formData[1].style.borderColor = "green";
+  }
+  else {
+    valid = false
+    formData[1].style.borderColor = "red";
+  }
 }
-// keep email data
+// email validation
 function emailData(e) {
-  console.log("emailData ->", e.target.value);
-  // TODO: maybe need regex for email validation !
+  console.log("birthdateData :", e.target.value);
+  var data = e.target.value;
+  var valid = false;
+  const regex_mail = RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
+  const regex_is = regex_mail.test(data);
+  console.log(regex_mail.test(data))
+  if (regex_is) {
+    valid = true;
+    formData[2].style.borderColor = "green";
+  } else {
+    valid = false;
+    formData[2].style.borderColor = "red";
+  }
 }
-// keep brithday date data
+// birthdate validation
 function birthdateData(e) {
-  console.log("birthdateData ->", e.target.value);
+  console.log("birthdateData :", e.target.value);
+  var data = e.target.value;
+  var valid = false;
+  const regex_date = RegExp(/^\d{2}[/]\d{2}[/]\d{4}$/);
+  const regex_is = regex_date.test(data);
+  if (regex_is) {
+    valid = true;
+    formData[3].style.borderColor = "green";
+  }
+  else {
+    valid = false
+    formData[3].style.borderColor = "red";
+  }
 }
-// kepp quantity data
+// quantity validation
 function quantityData(e) {
-  console.log("quantityData ->", e.target.value);
+  console.log("quantityData :", e.target.value);
+  var data = e.target.value;
+  var valid = false;
+  const regex_quantity = RegExp(/^\d{1,}$/);
+  const regex_is = regex_quantity.test(data);
+  console.log(regex_is);
+  if (regex_is) {
+    valid = true;
+    formData[4].style.borderColor = "green";
+  } else {
+    valid = false;
+    formData[4].style.borderColor = "red";
+  }
+}
+// location validation
+function locationData(e){
+  console.log(e.target.value);
+  var data = e.target.value;
+  var valid = false;
+  if (data != null) {
+    data = true;
+  } else {
+    valid = false;
+  }
+}
+// condition validation
+function conditionData(e) {
+  console.log(e.target.value);
 }
 
