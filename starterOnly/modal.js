64659bb34446b09310a93002a7e53d923b3b8cfb
Poccardl <loic.poccard@gmail.com>
2020-12-12 17:06:08 +0100

    Issue 3 100%

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -13,6 +13,7 @@ const closemodal = document.querySelector(".close");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData input");
 const pushform = document.querySelector(".btn-submit");
+const locationDiv = document.querySelector(".location_form");
 const locationCheckbox = document.querySelectorAll(".location_form [name=\"location\"]");
 
 // launch modal events
@@ -27,7 +28,7 @@ formData[2].addEventListener("change", emailData);
 formData[3].addEventListener("change", birthdateData);
 formData[4].addEventListener("change", quantityData);
 locationCheckbox.forEach((check) => check.addEventListener("change", locationData));
-console.log(formData)
+formData[11].addEventListener("change", conditionData);
 
 
 /* FUNCTION */
@@ -42,9 +43,8 @@ function closeModal() {
 //
 function pushForm() {
   console.log("push form");
-  formData[11].addEventListener("change", conditionData);
-  formData[12].addEventListener("change", newsletterData);
   locationData();
+  conditionData();
 }
 
 // first name validation
@@ -95,7 +95,7 @@ function emailData(e) {
   var valid = false;
   const regex_mail = RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/);
   const regex_is = regex_mail.test(data);
-  var error = ('<span id="error3" class="msg_error">Vous devez entrer une adresse email valide</span>');
+  var error = ('<span id="error3" class="msg_error">Vous devez entrer une adresse email valide.</span>');
   var errorData = document.getElementById("error3");
   if (regex_is) {
     valid = true;
@@ -136,7 +136,7 @@ function quantityData(e) {
   var valid = false;
   const regex_quantity = RegExp(/^\d{1,}$/);
   const regex_is = regex_quantity.test(data);
-  var error = ('<span id="error5" class="msg_error">Vous devez entrer un nombre</span>');
+  var error = ('<span id="error5" class="msg_error">Vous devez entrer un nombre.</span>');
   var errorData = document.getElementById("error5");
   if (regex_is) {
     valid = true;
@@ -152,42 +152,33 @@ function quantityData(e) {
 // location validation
 function locationData(e) {
   var valid = false;
-  var i = 0
-  console.log(locationCheckbox.length)
-  console.log(locationCheckbox)
-  console.log(locationCheckbox[0])
-  console.log(locationCheckbox[0].checked)
-  /*for (element in  locationCheckbox) {
+  for (element in  locationCheckbox) {
     if (locationCheckbox[element].checked == true) {
       valid = true;
     }
   };
-  */
- while (i < locationCheckbox.length) {
-  if (locationCheckbox[i].checked == true) {
-    console.log("test")
-    valid = true;
-  }
-  i++;
- };
-
-  var error = ('<span id="error6" class="msg_error">Vous devez choisir une ville</span>');
+  var error = ('<span id="error6" class="msg_error">Vous devez choisir une ville.</span>');
   var errorData = document.getElementById("error6");
-
   if (valid == true) {
     console.log("no error");
   } else {
     console.log("error");
-
+    locationDiv.insertAdjacentHTML("afterend", error);
   } try {
     errorData.remove();
   } catch {}
 }
-// condition validation
+// condition d'utilisation validation
 function conditionData(e) {
-  console.log(e.target.value);
-}
-// news newsletter
-function newsletterData() {
-  //console.log(e.target.value);
+  var valid = false;
+  var error = ('<span id="error7" class="msg_error">Vous devez acceptez les termes et conditions d’utilisation.</span>');
+  var errorData = document.getElementById("error7");
+  if (formData[11].checked == true) {
+    valid = true;
+  } else {
+    valid = false;
+    formData[12].insertAdjacentHTML("beforebegin", error);
+  } try {
+    errorData.remove();
+  } catch {}
 }
